22572b13a7e09b0244f48ca7e71ae4f77480c93d
Kartikey Pandey <kartikey224252@gmail.com>
2025-06-25 02:10:35 +0530

    fiz webhook

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,15 +1,47 @@
-import { Webhook } from "svix";
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import { NextRequest } from "next/server";
 import { headers } from "next/headers";
-import { WebhookEvent } from "@clerk/nextjs/server";
+import { Webhook } from "svix";
 
-export async function POST(req: Request) {
-  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
-  if (!WEBHOOK_SECRET) {
-    throw new Error("Please add WEBHOOK secret from clerk");
+export async function POST(req: NextRequest) {
+  try {
+    const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
+    if (!WEBHOOK_SECRET) {
+      throw new Error("please add clerk webhook secret from clerk dashboard ");
+    }
+    const evt = await verifyWebhook(req);
+
+    // Do something with payload
+    //
+    // getting the headers
+    const headerPayload = await headers();
+    const svix_id = headerPayload.get("svix-id");
+    const svix_timestamp = headerPayload.get("svix-timestamp");
+    const svix_signature = headerPayload.get("svix-signature");
+
+    if (!svix_id || !svix_timestamp || !svix_signature) {
+      return new Response("Missing svix headers", { status: 400 });
+    }
+
+    const payload = await req.json();
+    const body = JSON.stringify(payload);
+    const wh = new Webhook(WEBHOOK_SECRET);
+    wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+    });
+
+    // For this guide, log payload to console
+    const { id } = evt.data;
+    const eventType = evt.type;
+    console.log(
+      `Received webhook with ID ${id} and event type of ${eventType}`,
+    );
+    console.log("Webhook payload:", evt.data);
+
+    return new Response("Webhook received", { status: 200 });
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error verifying webhook", { status: 400 });
   }
-
-  const headerPayload = headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
 }
