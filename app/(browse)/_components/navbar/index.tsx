429e85ec48d80962b2c1303586aa88fc061b0e27
Kartikey Pandey <kartikey224252@gmail.com>
2025-06-29 14:23:37 +0530

    done

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -1,11 +1,13 @@
 import { TwitchLogo } from "@/app/(auth)/_components/logo";
 import { Search } from "./search";
+import { Actions } from "./Actions";
 
 export const Navbar = () => {
   return (
     <nav className="fixed top-0 w-full h-20 z-[49] bg-[#151517] px-2 lg:px-4 flex justify-between items-center shadow-sm">
       <TwitchLogo />
       <Search />
+      <Actions />
     </nav>
   );
 };
--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -1,16 +1,53 @@
 "use client";
 import { Input } from "@/components/ui/input";
-import { SearchIcon } from "lucide-react";
+import { SearchIcon, X } from "lucide-react";
 import qs from "query-string";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+//import { RouterContext } from "next/dist/shared/lib/router-context.shared-runtime";
 
 export const Search = () => {
+  const router = useRouter();
+  const [value, setValue] = useState("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!value) return;
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: { term: value },
+      },
+      { skipEmptyString: true }
+    );
+
+    router.push(url);
+  };
+
+  const onClear = () => {
+    setValue("");
+  };
+
   return (
-    <form className="relative w-full lg:w-[400px] flex items-center">
-      <Input 
-        placeholder="Search" 
-        className="bg-[#313338] text-white placeholder:text-white border-0  placeholder:text-sm placeholder:text-muted-foreground"
+    <form
+      onSubmit={onSubmit}
+      className="relative w-full lg:w-[400px] flex items-center"
+    >
+      <Input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Search"
+        className="bg-[#313338] text-white border-0  placeholder:text-sm placeholder:text-muted-foreground"
       />
+      {value && (
+        <X
+          className="absolute top-2.5 right-14 h-5 w-5 text-muted-foreground cursor-pointer hover:opacity-75 transition-shadow"
+          onClick={onClear}
+        />
+      )}
 
       <Button
         type="submit"
